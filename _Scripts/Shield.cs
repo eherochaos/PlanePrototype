698eaec3fd646a3fe5834d7e9bf51ed2e1b43d9d
eherochaos <58042432+eherochaos@users.noreply.github.com>
2020-04-24 10:10:49 +0800

    Add files via upload

---
+++ _Scripts/Shield.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float RotationsPerSecond = 0.1f;
+
+    public bool ______________;
+
+    public int LevelShown = 0;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var currentLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+
+        if (this.LevelShown != currentLevel)
+        {
+            this.LevelShown = currentLevel;
+            var mat = this.GetComponent<Renderer>().material;
+            mat.mainTextureOffset = new Vector2(0.2f*this.LevelShown,0);
+        }
+
+        var rZ = (this.RotationsPerSecond * Time.time * 360) % 360f;
+        this.transform.rotation = Quaternion.Euler(0,0,rZ);
+
+    }
+}
